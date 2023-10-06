73796fae23524a14e49f6b2419386e7a39f31416
rafael.dev <rafaelvillagomez.dev@gmail.com>
2023-10-06 22:12:06 +0200

    Configuracion funcionando en webpack , babel

---
+++ config/webpack.dev.js
@@ -0,0 +1,19 @@
+const common=require("./webpack.common")
+const path = require("path");
+const {merge}=require("webpack-merge")
+const devConfig={
+    mode:process.env.NODE_ENV || "development",
+    devServer: {
+        static: {
+          directory: path.join(__dirname, './dist'),
+        },
+        compress: true,
+        port: 9000,
+        hot:true,
+        historyApiFallback: true,
+      },
+    devtool:"eval-source-map"
+    
+}
+
+module.exports=merge(common,devConfig);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React  from "react";
+
+
+function App() {
+
+
+  return (
+    
+      <div id="app" className="App">
+       <h1>APP</h1>
+ 
+      </div>
+   
+  );
+}
+
+export default App;
